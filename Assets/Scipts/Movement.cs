c7f98e5f11c80cf9aa4589db1ea21e0b61c4c169
nikitazaicev@inbox.lv <nikita13391>
2020-01-07 19:31:44 +0100

    init
    test

---
+++ Assets/Scipts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal")* 50;
+        float v = Input.GetAxis("Vertical")  * 50;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 5000000);
+        }
+        
+
+        Vector3 vel = rb.velocity;
+        vel.x = h;
+        vel.z = v;
+        rb.velocity = vel;
+
+        
+    }
+}
---
+++ Assets/Scipts/mouseThings.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseThings : MonoBehaviour
+{
+
+    float speed = 200.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 300.0f))
+        {
+            if (hit.transform)
+            {
+                    GameObject go;
+                    Rigidbody rb;
+                    if (rb = hit.transform.GetComponent<Rigidbody>())
+                    {
+                        Print(hit.transform.gameObject);
+                        launchUp(rb);
+                    }
+            }
+        }
+        }
+    }
+
+    void Print(GameObject go)
+    {
+        print(go);
+    }
+
+    void launchUp(Rigidbody rb)
+    {
+        rb.AddForce(transform.up*speed, ForceMode.Impulse);
+    }
+
+}
